9b5b16f24267bc44fa4c2dd722b7b5efb420702e
Filipe Teixeira <filipe942001teixeira@gmail.com>
2024-02-22 11:19:04 +0000

    Added env variable

--- src/pages/Cards/CardCreate.jsx
+++ src/pages/Cards/CardCreate.jsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import JSON_SERVER_LINK from "../../data/json-backend-cards-created";
 import { useNavigate } from "react-router-dom";
 
-const OPEN_API_KEY = "OPEN_AI_KEY";
 
 function CardCreate() {
   const [name, setName] = useState("");
@@ -24,6 +23,7 @@ function CardCreate() {
   const [imageURL, setImageURL] = useState("")
 
   async function generateImages() {
+    console.log(import.meta.env.VITE_API_KEY)
     setIsLoading(true);
 
     try {
@@ -35,7 +35,7 @@ function CardCreate() {
 
       const headers = {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${OPEN_API_KEY}`,
+        Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
       };
 
       const response = await axios.post(
--- src/pages/Cards/CardsList.jsx
+++ src/pages/Cards/CardsList.jsx
@@ -1,78 +1,3 @@
-/* import React from 'react'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import APIURL from '../../data/mtg-api'
-import { Link } from 'react-router-dom'
-
-const API = APIURL()
-
-function CardsList() {
-
-    const [cards, setCards] = useState([]);
-
-    useEffect(() => {
-        axios
-            .get(`${API}/search?q=c`)
-            .then((response) => {
-                setCards(response.data.data)
-                console.log(response.data.data)
-            })
-            .catch((error) => { console.log(error) })
-    }, [])
-
-    const cardsWithImage = cards.filter((card) => { return (card.image_uris?.normal) })
-
-/*     function filter(color) {
-        console.log(cards[0] && cards[0].colors);
-
-        let filteredCards = []
-
-        cards && cards.forEach(card => {
-            card && card.colors.forEach((colour)=>{
-                if(colour === color){
-                    filteredCards.push(card)
-                }
-            })
-        });
-
-        setCards(filteredCards)
-    } 
-
-    return (
-                <div className='cardsListPage'>
-                    <h1>Cards List</h1>
-                    <section className='lists'>
-                        <Link to={"/custom-cards"}>
-                            <p>Custom Cards List</p>
-                        </Link>
-                    </section>
-                    <section className='cardsFilters'>
-                        <p>Here there will be filters</p>
-                        <button>Black</button>
-                        <button>Blue</button>
-                        <button>Green</button>
-                        <button>Red</button>
-                        <button>White</button>
-                        <button>Artifact</button>
-                    </section>
-
-                    <div className="cardsDisplay">
-                        {cardsWithImage.length > 0 &&
-                            cardsWithImage.map(card => (
-                                <Link to={`/cards/${card.id}`}>
-                                    <img key={card.id} src={card.image_uris?.normal} alt={card.name} />
-                                </Link>
-                            ))
-                        }
-                    </div>
-                    
-                </div>
-    );
-}
-
-export default CardsList */
-
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
@@ -86,8 +11,10 @@ function CardsList() {
 
     useEffect(() => {
         axios.get(`${API}/search?q=c`)
-            .then(response => {
-                setCards(response.data.data.filter(card => card.image_uris?.normal));
+                .then(response => {
+                    const filteredData = response.data.data.filter(card => card.image_uris?.normal);
+                    setCards(filteredData);
+                    setFilteredCards(filteredData);
             })
             .catch(error => {
                 console.error('Error fetching cards:', error);
@@ -98,7 +25,11 @@ function CardsList() {
         if (color === 'All') {
             setFilteredCards(cards);
         } else {
-            setFilteredCards(cards.filter(card => card.colors?.includes(color)));
+            if (color.length === 0) {
+                setFilteredCards(cards.filter(card => !card.colors || card.colors.length === 0));
+            } else {
+                setFilteredCards(cards.filter(card => card.colors?.includes(color)));
+            }
         }
     };
 
@@ -116,13 +47,40 @@ function CardsList() {
             </section>
             <section className="cardsFilters">
                 <p>Filter by Color:</p>
-                <button className="btn-31" onClick={() => handleFilterClick('All')}>All</button>
-                <button className="btn-31" onClick={() => handleFilterClick('B')}>Black</button>
-                <button className="btn-31" onClick={() => handleFilterClick('U')}>Blue</button>
-                <button className="btn-31" onClick={() => handleFilterClick('G')}>Green</button>
-                <button className="btn-31" onClick={() => handleFilterClick('R')}>Red</button>
-                <button className="btn-31" onClick={() => handleFilterClick('W')}>White</button>
-                <button className="btn-31" onClick={() => handleFilterClick('Artifact')}>Artifact</button>
+                <button className="btn-31" onClick={() => handleFilterClick('All')}>
+                    <span className="text-container">
+                        <span className="text">All</span>
+                    </span>
+                </button>
+                <button className="btn-31" onClick={() => handleFilterClick('B')}>
+                    <span className="text-container">
+                        <span className="text">Black</span>
+                    </span>
+                </button>
+                <button className="btn-31" onClick={() => handleFilterClick('U')}>
+                    <span className="text-container">
+                        <span className="text">Blue</span>
+                    </span>
+                </button>
+                <button className="btn-31" onClick={() => handleFilterClick('G')}>
+                    <span className="text-container">
+                        <span className="text">Green</span>
+                    </span>
+                </button>
+                <button className="btn-31" onClick={() => handleFilterClick('R')}>
+                    <span className="text-container">
+                        <span className="text">Red</span>
+                    </span>
+                </button>
+                <button className="btn-31" onClick={() => handleFilterClick('W')}>
+                    <span className="text-container">
+                        <span className="text">White</span>
+                    </span></button>
+                <button className="btn-31" onClick={() => handleFilterClick([])}>
+                    <span className="text-container">
+                        <span className="text">Colorless</span>
+                    </span>
+                </button>
             </section>
             <div className="cardsDisplay">
                 {filteredCards.length > 0 ? (
